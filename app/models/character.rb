ba5ace6a2283e29c70026c7b23bb3a3f5ef594e8
Hector_Jimenez <hjmontemayor@gmail.com>
2020-11-09 01:19:45 -0500

    Done.

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,15 @@
 class Character < ActiveRecord::Base
-  
+    attr_accessor :catchphrase
+
+    belongs_to :actor
+    belongs_to :show
+
+    def catchphrase
+
+    end
+
+    def say_that_thing_you_say
+        "#{self.name} always says: #{self.catchphrase}"
+    end
+
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,12 @@
 class Show < ActiveRecord::Base
-  
+    belongs_to :network
+    has_many :characters
+    has_many :actors, through: :characters
+    #has_many :networks, through: :shows
+
+    def actors_list
+        self.actors.collect do |i|
+            i.full_name
+        end
+    end
 end
